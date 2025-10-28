7f1faa58e8ae4fe134d26012293c09d9febeadca
Filipe Cavalcanti <filipecavalcantiximenes@gmail.com>
2025-10-28 19:27:35 -0300

    :sparkles: added: task entity

--- src/domain/task.ts
+++ src/domain/task.ts
@@ -1,3 +1,41 @@
-export class Task {
-    
+import { InvalidValueError } from "./Errors/InvalidValueError.ts";
+
+export interface TaskProps {
+    id: string;
+    title: string;
+    completed: boolean;
 }
+export class Task {
+    constructor(private taskProps: TaskProps){
+        this.validateTitle(taskProps.title)
+    }
+
+    get id(): string {
+        return this.taskProps.id;
+    }
+
+    get title(): string {
+        return this.taskProps.title;
+    }
+    
+
+    get completed(): boolean {
+        return this.taskProps.completed;
+    }
+
+    validateTitle(title: string): boolean {
+        if (title.length < 1) {
+            throw new InvalidValueError('Você deve inserir um título')
+        }
+        return true;
+    }
+    changeTitle(newTitle: string) {
+        this.validateTitle(newTitle);
+        this.taskProps.title = newTitle;
+    }
+
+    changeStatus(): boolean {
+        return this.taskProps.completed != this.taskProps.completed;
+    }
+}
+
